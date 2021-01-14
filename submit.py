484c0b01314f394021030aa10a6ad7553f7d19d7
hiromi-mi <hiromi-mi@cat.zaq.jp>
2021-01-15 02:57:12 +0900

    submit.py: get time as localtime

--- submit.py
+++ submit.py
@@ -131,8 +131,9 @@ def submit():
 @login_required
 def get_submissions():
     db = get_db()
+    # datetime(created, "localtime") created -> get time as localtime
     submissions = db.execute(
-            'SELECT s.id, fieldname, length, username, status, created'
+            'SELECT s.id, fieldname, length, username, status, datetime(created, "localtime") created'
             ' FROM submission s JOIN user u ON s.user_id = u.id'
             ' ORDER BY created DESC'
             ).fetchall()
@@ -143,7 +144,7 @@ def get_submissions():
 def get_submission(submitid):
     db = get_db()
     submission = db.execute(
-            'SELECT fieldname, length, username, status, created, source'
+            'SELECT fieldname, length, username, status, datetime(created, "localtime") created, source'
             ' FROM submission s JOIN user u ON s.user_id = u.id'
             ' WHERE s.id = ?'
             ,(submitid,)
