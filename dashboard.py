976dbbd769cb741a2e42d739ff508b13d0a064a5
hiromi-mi <hiromi-mi@cat.zaq.jp>
2021-01-21 18:34:56 +0900

    dashboard で言語ごとに取得していた N+1 を解消

--- dashboard.py
+++ dashboard.py
@@ -11,17 +11,21 @@ def dashboard():
     field_bests = []
     # TODO N+1
     # at least one item are required
-    for cnt, field in enumerate(current_app.config['FIELDS'].values()):
-        smallest_submission = db.execute(
-                'SELECT submission.id, length, username, user_id'
-                ' FROM submission JOIN user ON user.id = submission.user_id'
-                ' WHERE fieldname = ?'
-                " and status = 'AC'"
-                ' ORDER BY length ASC',
-                (field["fieldname"],),
-                ).fetchone()
-        if smallest_submission is not None:
-            field_bests.append((field['fieldname'], f"{smallest_submission['length']} Bytes", f"user-{smallest_submission['username']}", cnt, smallest_submission['user_id']))
-        else:
-            field_bests.append((field['fieldname'], "", "", cnt))
+    field_bests = db.execute(
+            'select min(length), source, fieldname, username, user_id'
+            ' from submission join user on user.id = submission.user_id'
+            ' where status = "AC" group by fieldname')
+#    for cnt, field in enumerate(current_app.config['fields'].values()):
+#        smallest_submission = db.execute(
+#                'select submission.id, length, username, user_id'
+#                ' from submission join user on user.id = submission.user_id'
+#                ' where fieldname = ?'
+#                " and status = 'ac'"
+#                ' order by length asc',
+#                (field["fieldname"],),
+#                ).fetchone()
+#        if smallest_submission is not none:
+#            field_bests.append((field['fieldname'], f"{smallest_submission['length']} Bytes", f"user-{smallest_submission['username']}", cnt, smallest_submission['user_id']))
+#        else:
+#            field_bests.append((field['fieldname'], "", "", cnt))
     return render_template('index.html', field_bests=field_bests)
