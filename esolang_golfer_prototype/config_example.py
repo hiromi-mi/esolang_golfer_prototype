a3bbbb1c1e1111878f29f5740f9b88ac7bda3aa4
hiromi-mi <hiromi-mi@cat.zaq.jp>
2021-03-19 05:10:14 +0900

    add config_example

---
+++ esolang_golfer_prototype/config_example.py
@@ -0,0 +1,3 @@
+PROBLEM = "テスト問題"
+TESTCASES = [("input_example", "output_example")]
+FIELDS = {"rust": {"fieldname": "rust", "fieldlangs": ["rust"]}, "Brainfuck/C++": {"fieldname": "Brainfuck/C++", "fieldlangs": ["brainfuck-bfi", "cpp-clang"]}}
